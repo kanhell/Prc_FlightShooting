9080c65eba976224bc6babea58b3067de9c36045
kanhell <kanhelll@naver.com>
2025-11-07 16:07:43 +0900

    Boss Bullet
    - Phase 1 : LAttack
    - Phase 2 : RAttack x3
    - Boss Attacked Animation : Color Red

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,7 +14,7 @@ public class BossController : MonoBehaviour
     //
     Animator animator;
     //
-    bool onDead;
+    bool onDead; 
     bool isSpawn;
     //
     int score;
@@ -22,6 +23,19 @@ public class BossController : MonoBehaviour
     //
     Transform spawnMovePos;
     float speed;
+
+    // 총알
+    public Transform LAttackPos;
+    public Transform RAttackPos;
+    public GameObject bossBullet;
+    float fireDelay;
+
+    // 애니메이션 상태
+    int animNumber;  // -1: 대기.이동반복, 0: 대기.이동, 1: L공격, 2: R공격, 3: Die
+
+    // v피격
+    public SpriteRenderer spriteRenderer;
+    Color currentColor;
 
 
     void Awake()
@@ -44,9 +58,13 @@ public class BossController : MonoBehaviour
         score = 1000;
 
         speed = 10;
+
+        animNumber = 0;
+
+        currentColor = spriteRenderer.color;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (isSpawn)
@@ -55,6 +73,101 @@ public class BossController : MonoBehaviour
             time += Time.deltaTime;
         if (time > 0.6f)
             Destroy(gameObject);
+        if (player == null && GameManager.instance.lifeCount >= 0)
+            PlayerFind();
+        FireBullet();
+        AnimationSystem();
+    }
+
+    public void PlayerFind()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent <PlayerController>();
+    }
+
+    void FireBullet()
+    {
+        // 페이즈 1 (초록 hp바)
+        if (hp1 > 0 && isSpawn == false)
+        {
+            fireDelay += Time.deltaTime;
+            if (fireDelay > 1.0f && animNumber != 1)
+            {
+                // L공격
+                animNumber = 1;
+                fireDelay -= fireDelay;
+            }
+        }
+        // 페이즈 2 (빨강 hp바)
+        if (hp1 <= 0)
+        {
+            fireDelay += Time.deltaTime;
+            if (fireDelay > 1.0f && animNumber != 2)
+            {
+                // R공격
+                animNumber = 2;
+                fireDelay -= fireDelay;
+            }
+        }
+    }
+
+    void AnimationSystem()
+    {
+        if (animNumber == 0)
+        {
+            StartCoroutine(Co_Idle());
+        }
+        if (animNumber == 1)
+        {
+            StartCoroutine(Co_LAttack());
+        }
+        if (animNumber == 2)
+        {
+            StartCoroutine(Co_RAttack());
+        }
+    }
+
+    IEnumerator Co_Idle()
+    {
+        animNumber = -1;
+        animator.SetTrigger("Idle");
+        yield return new WaitForSeconds(0.6f);
+    }
+
+    IEnumerator Co_LAttack()
+    {
+        animNumber = -1;
+        animator.SetTrigger("LAttack");
+        yield return new WaitForSeconds(0.6f);
+        animNumber = 0;
+    }
+
+    IEnumerator Co_RAttack()
+    {
+        animNumber = -1;
+        animator.SetTrigger("RAttack");
+        yield return new WaitForSeconds(0.6f);
+        animator.SetTrigger("RAttack");
+        yield return new WaitForSeconds(0.6f);
+        animator.SetTrigger("RAttack");
+        yield return new WaitForSeconds(0.6f);
+        animNumber = 0;
+    }
+
+    void LAttack()
+    {
+        if (player == null)
+            return;
+        Instantiate(bossBullet, LAttackPos.position, Quaternion.identity);
+        fireDelay -= 1f;
+    }
+
+    void RAttack()
+    {
+        if (player == null)
+            return;
+        Instantiate(bossBullet, RAttackPos.position, Quaternion.identity);
+        fireDelay -= 1f;
     }
 
     private void OnDead()
@@ -84,6 +197,7 @@ public class BossController : MonoBehaviour
                 hp1 = hp1 - playerController.Damage;
             else
                 hp2 = hp2 - playerController.Damage;
+            StartCoroutine(OnDemageEffect());
         }
         if (collision.CompareTag("BoomMissile"))
         {
@@ -91,12 +205,20 @@ public class BossController : MonoBehaviour
                 hp1 = hp1 - playerController.BoomDamage;
             else
                 hp2 = hp2 - playerController.BoomDamage;
+            StartCoroutine(OnDemageEffect());
         }
         if (hp2 <= 0)
         {
             animator.SetTrigger("Die");
             OnDead();
         }
+    }
+
+    IEnumerator OnDemageEffect()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = currentColor;
     }
 
 }
